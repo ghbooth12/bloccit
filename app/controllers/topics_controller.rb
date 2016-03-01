95433265d9c940a0d2ac6cb09ad344fde5eed5a7
Gahee Heo <ghbooth12@gmail.com>
2016-02-29 22:52:17 -0600

    checkpoint-34-topics-posts: completed

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,51 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash[:error] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+
+    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :resolved))
+      redirect_to @topic, notice: "Topic was updated."
+    else
+      flash[:error] = "Error updating topic. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash[:error] = "Error deleting topic. Please try again."
+      render :show
+    end
+  end
+end
