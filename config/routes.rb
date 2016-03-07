a6595a83d1bcb14da8ab0d65dc3cd3b0913ac9e7
Gahee Heo <ghbooth12@gmail.com>
2016-03-06 23:15:59 -0600

    assignment-42-labels: completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
+  resources :topics, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
   end
