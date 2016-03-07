9fc8efe11c1063a9cb086fb9e8c165387be1944f
Gahee Heo <ghbooth12@gmail.com>
2016-03-06 21:14:04 -0600

    checkpoint-42-labels: completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -18,6 +18,7 @@ class TopicsController < ApplicationController
     @topic = Topic.new(topic_params)
 
     if @topic.save
+      @topic.labels = Label.update_labels(params[:topic][:labels])
       redirect_to @topic, notice: "Topic was saved successfully."
     else
       flash.now[:alert] = "Error creating topic. Please try again."
@@ -34,6 +35,7 @@ class TopicsController < ApplicationController
     @topic.assign_attributes(topic_params)
 
     if @topic.save
+      @topic.labels = Label.update_labels(params[:topic][:labels])
       redirect_to @topic, notice: "Topic was updated."
     else
       flash.now[:alert] = "Error updating topic. Please try again."
